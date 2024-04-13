333e0aef4cb35cc24b5eb199917a852cd65edb9d
zaflaton <59338890+zaflaton@users.noreply.github.com>
2024-04-13 10:37:16 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Hihglights from './components/Highlights'
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Hihglights />
+    </main>
+  )
+}
+
+export default App
